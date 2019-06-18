76aa7aaeecd91724947a41675b046df1eb6c5327
ramkishor05 <ramkishor05@gmail.com>
2019-06-18 09:58:24 +0530

    ramkishor05

--- src/main/java/org/brijframework/support/model/Model.java
+++ src/main/java/org/brijframework/support/model/Model.java
@@ -7,7 +7,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.brijframework.support.enums.Scope;
 import org.brijframework.util.support.Access;
 import org.brijframework.util.support.Constants;
 
@@ -21,21 +20,7 @@ public @interface Model {
 
 	Access access() default Access.PUBLIC;
 
-	Scope scope() default Scope.PROTOTYPE;
-	
 	String extend() default Constants.DEFAULT;
 	
 	Construct constructor() default @Construct;
-	
-	String[] methodKeys() default {};
-	
-	String[] propertyKeys() default {};
-	
-	boolean isUseDefaultFields() default true;
-	
-	boolean isUseDefaultMethods() default false;
-	
-	boolean isUseMappedKeys() default false;
-	
-	boolean isDefault() default false;
 }
